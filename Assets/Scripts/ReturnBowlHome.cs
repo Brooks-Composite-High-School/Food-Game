68b781dde58ef561690fceeb4390b87de4ea3a13
Connor-deBoer <75091148+Connor-deBoer@users.noreply.github.com>
2020-11-26 11:35:33 -0700

    Add files via upload

---
+++ Assets/Scripts/ReturnBowlHome.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ReturnBowlHome : MonoBehaviour
+{
+    Vector3 homeVector;
+    Quaternion homeQuad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        homeVector = transform.position;
+        homeQuad = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = homeVector;
+        transform.rotation = homeQuad;
+    }
+}
